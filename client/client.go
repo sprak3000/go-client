45ef0b2becf2a2a2c1d5840ea363f2a14314684b
Kyle <kyle.boorky@gmail.com>
2017-02-16 15:50:14 -0500

    adding base client

---
+++ client/client.go
@@ -0,0 +1,91 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+
+	"encoding/json"
+
+	"time"
+
+	"github.com/healthimation/go-glitch/glitch"
+)
+
+// Error codes
+const (
+	ErrorCantFind         = "CANT_FIND_SERVICE"
+	ErrorRequestCreation  = "CANT_CREATE_REQUEST"
+	ErrorRequestError     = "ERROR_MAKING_REQUEST"
+	ErrorDecodingError    = "ERROR_DECODING_ERROR"
+	ErrorDecodingResponse = "ERROR_DECODING_RESPONSE"
+)
+
+// ServiceFinder can find a service's base URL
+type ServiceFinder func(serviceName string, useTLS bool) (url.URL, error)
+
+// BaseClient can do requests
+type BaseClient interface {
+	Do(ctx context.Context, method string, slug string, headers http.Header, body io.Reader, response interface{}) glitch.DataError
+}
+
+type client struct {
+	finder      ServiceFinder
+	useTLS      bool
+	serviceName string
+	client      *http.Client
+}
+
+// NewBaseClient creates a new BaseClient
+func NewBaseClient(finder ServiceFinder, serviceName string, useTLS bool, timeout time.Duration) BaseClient {
+	c := &http.Client{Timeout: timeout}
+	result := &client{finder: finder, serviceName: serviceName, useTLS: useTLS, client: c}
+}
+
+func (c *client) Do(ctx context.Context, method string, slug string, headers http.Header, body io.Reader, response interface{}) glitch.DataError {
+	u, err := c.finder(c.serviceName, c.useTLS)
+	if err != nil {
+		return glitch.NewDataError(err, ErrorCantFind, "Error finding service")
+	}
+	u.Path = slug
+
+	req, err := http.NewRequest(method, u.String(), body)
+	if err != nil {
+		return glitch.NewDataError(err, ErrorRequestCreation, "Error creating request object")
+	}
+
+	if ctx != nil {
+		req = req.WithContext(ctx)
+	}
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return glitch.NewDataError(err, ErrorRequestError, "Could not make the request")
+	}
+	defer func() {
+		io.Copy(ioutil.Discard, resp.Body)
+		resp.Body.Close()
+	}()
+
+	dec := json.NewDecoder(resp.Body)
+	if resp.StatusCode >= 400 || resp.StatusCode < 200 {
+		prob := glitch.HTTPProblem{}
+		err = dec.Decode(&prob)
+		if err != nil {
+			return glitch.NewDataError(err, ErrorRequestError, "Could not decode error response")
+		}
+		return glitch.FromHTTPProblem(prob, fmt.Sprintf("Error from %s to %s - %s", method, c.serviceName, slug))
+	}
+
+	if response != nil {
+		err = dec.Decode(response)
+		if err != nil {
+			return glitch.NewDataError(err, ErrorDecodingResponse, "Could not decode response")
+		}
+	}
+
+	return nil
+}
